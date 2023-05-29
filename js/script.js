a65e7217f7eacce743ab2f30560267103a9ec721
Exx1piee <mishapron2008@gmail.com>
2023-05-29 21:57:31 +0300

    bcb

--- js/script.js
+++ js/script.js
@@ -1,11 +1,1 @@
-const openModalBtn = document.getElementById('open-modal');
-const closeModalBtn = document.getElementById('close-modal');
-const modal = document.getElementById('modal');
-
-openModalBtn.addEventListener('click', () => {
-   modal.style.display = 'block';
-});
-
-closeModalBtn.addEventListener('click', () => {
-   modal.style.display = 'none';
-});
+.heroimg-window is-hidden;
