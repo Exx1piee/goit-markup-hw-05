8daac4b73925349647160bc6f37764ea40d3e6c9
Exx1piee <mishapron2008@gmail.com>
2023-05-29 21:42:11 +0300

    s

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-function showModal() {
-    document.getElementById("myModal").style.display = "block";
-  }
+const openModalBtn = document.getElementById('open-modal');
+const closeModalBtn = document.getElementById('close-modal');
+const modal = document.getElementById('modal');
+
+openModalBtn.addEventListener('click', () => {
+   modal.style.display = 'block';
+});
+
+closeModalBtn.addEventListener('click', () => {
+   modal.style.display = 'none';
+});
